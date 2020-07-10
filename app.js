218b66df47d0b38be304e80723209b7af03b5bbc
liondon <liondonaugust810810@gmail.com>
2020-07-10 01:35:44 -0400

    add user index page & admin index page, initialize sequelize

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-
+const db = require('./models')
 const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engin', 'hbs')
-
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+app.set('view engine', 'hbs')
 
 app.listen('3000', () => {
+  db.sequelize.sync()
   console.log(`Express server is running on http://localhost:3000`)
 })
+
+require('./routes')(app)
