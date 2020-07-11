dd999955d1f7585feefff476fa20b4280db1cbe6
liondon <liondonaugust810810@gmail.com>
2020-07-11 00:25:02 -0400

    feat: add admin/restaurants page

---
+++ migrations/20200711040034-create-restaurant.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Restaurants', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      tel: {
+        type: Sequelize.STRING
+      },
+      addr: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      open_hours: {
+        type: Sequelize.STRING
+      },
+      desc: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Restaurants');
+  }
+};
