63f9f0ba96de77b54644a01e86fa70ac8a7da8d4
liondon <liondonaugust810810@gmail.com>
2020-07-18 14:54:30 -0400

    feat: add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,25 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: async (req, res) => {
+    const restaurants = await Restaurant.findAll({
+      include: [Category],
+      raw: true,
+      nest: true
+    })
+    const restData = restaurants.map(r => ({
+      ...r,
+      desc: r.desc.substring(0, 50)
+    }))
+    const categories = await Category.findAll({
+      include: [Restaurant],
+      raw: true,
+      nest: true
+    })
+
+    return res.render('restaurants', { restaurants: restData, categories })
   }
 }
 
